57cbaedc4ededac49cc5109b0c6cd27404088867
davivsouza <davivasconcelossouza7@gmail.com>
2025-05-26 16:15:10 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Disaster Watch",
+  description:
+    "O Disaster Watch Monitor é uma plataforma digital desenvolvida com o objetivo de fornecer uma visão clara e acessível sobre eventos extremos que ocorrem ao redor do globo. Em um mundo onde desastres naturais como incêndios, tempestades e terremotos são cada vez mais frequentes e impactantes, esta ferramenta busca centralizar informações de fontes confiáveis, apresentando-as de forma intuitiva para conscientizar e informar o público.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
